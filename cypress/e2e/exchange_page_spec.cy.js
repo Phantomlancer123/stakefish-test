9375323ce1905d2ab920cdf3265e2a7889a1ce25
Phantomlancer123 <dracula1128bat@gmail.com>
2022-06-06 22:32:17 +0900

    check go to detail

--- cypress/e2e/exchange_page_spec.cy.js
+++ cypress/e2e/exchange_page_spec.cy.js
@@ -8,4 +8,13 @@ describe('exchange_page_spec.cy.js', () => {
     it('show high-level information (name, country, URL, logo, trust rank, trust score)', () => {
         cy.get('.ant-table-row').find('td').should('have.length', 60)
     })
+    it('details button click', () => {
+        cy.get('.ant-table-tbody')
+            .find('tr')
+            .eq(1)
+            .find('td')
+            .eq(0)
+            .find('.name-form')
+            .click()
+    })
 })
